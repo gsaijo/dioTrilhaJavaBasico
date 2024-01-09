2fcc25313cc50fc61b125bcd8c3d0fbc8a302dab
Saijo <gsaijo@protonmail.com>
2024-01-09 01:21:45 -0300

    classe scanner adicionada junto das variaveis

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TO DO:
+        // conhecer e importar a classe Scanner
+        // exibir as mensagens para o nosso usuário
+        // obter pela classe Scanner os valores digitados no terminal
+        // exibir a mensagem final
+
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.print("Por favor, digite seu nome: ");
+            String nome = scanner.nextLine();
+
+            System.out.print("Por favor, digite o numero da conta: ");
+            int numero = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.print("Por favor, digite o numero de agencia: ");
+            String agencia = scanner.nextLine();
+
+            System.out.print("Por favor, digite o saldo: ");
+            double saldo = scanner.nextDouble();
+
+            System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está dsponível para saque!");
+        }
     }
 }
